acba275a1079320169627adea651299e994e597f
jashaikh-dev <junaidazharshaikh@gmail.com>
2020-08-15 21:51:48 -0400

    feat: updated dependencies

--- App.js
+++ App.js
@@ -1,26 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Login from './src/screens/Auth/Login';
 import Register from './src/screens/Auth/Register'
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hayya A'las-salah</Text>
-      
-      <Register />
-      <Login />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const nav = createStackNavigator(
+  {
+    Login: Login,
+    Register: Register
   },
-});
+  {
+    initialRouteName: "Login",
+    defaultNavigationOptions: {
+      title: "App"
+    }
+  }
+);
+
+export default createAppContainer(nav);
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hayya A'las-salah</Text>
+      
+//       <Register />
+//       <Login />
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
